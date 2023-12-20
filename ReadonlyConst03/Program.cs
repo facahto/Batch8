a87135f15dac6d775424a0369f82341710579d2c
facahto <fcahyohartoto@gmail.com>
2023-12-20 16:53:16 +0700

    change03

---
+++ ReadonlyConst03/Program.cs
@@ -0,0 +1,20 @@
+﻿class Car
+{
+	public readonly int year;
+	public const string brand = "Honda";
+	
+	public Car(int year)
+	{
+		this.year = year;
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		Car car = new Car(2008);
+		Console.WriteLine(car.year);
+	}
+}
+
