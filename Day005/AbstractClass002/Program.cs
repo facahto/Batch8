5cc21807fb84332f063f606fe54205a60d2ccd8b
facahto <fcahyohartoto@gmail.com>
2023-12-26 15:58:58 +0700

    Day05

---
+++ Day005/AbstractClass002/Program.cs
@@ -0,0 +1,48 @@
+﻿class EducatorChecker
+{
+	public void CheckCollege(Student student)
+	{
+		Console.WriteLine(student.College());
+	}
+	public void CheckHighSchool(Student student)
+	{
+		Console.WriteLine(student.HighSchool());
+	}
+}
+abstract class Student
+{
+	public abstract string HighSchool();
+	public abstract string College();
+}
+class JakartaStudent : Student
+{
+	public override string HighSchool()
+	{
+		return "SMA 1 Jakarta";
+	}
+	public override string College()
+	{
+		return "Binus";
+	}
+	
+}
+class JogjakartaStudent : Student
+{
+	public override string HighSchool()
+	{
+		return "DB2";
+	}
+	public override string College()
+	{
+		return "UGM";
+	}
+}
+class Program
+{
+	static void Main()
+	{
+		EducatorChecker education = new EducatorChecker();
+		education.CheckCollege(new JakartaStudent());
+		education.CheckHighSchool(new JogjakartaStudent());
+	}
+}
