813bfe8fa98628656f7b85e439e72099514aac71
facahto <fcahyohartoto@gmail.com>
2023-12-26 17:03:13 +0700

    Day05

---
+++ Day005/noname/Program.cs
@@ -0,0 +1,16 @@
+﻿class Car
+{
+	static int count;
+	public int price = 5;
+	public void GetPrice()
+	{
+		Console.WriteLine(price);
+	}
+}
+class Program
+{
+	static void Main()
+	{
+		Car.GetPrice();
+	}
+}
