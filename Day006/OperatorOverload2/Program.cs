b2b9f555d9e0fe733ebf8ed38d93a6032d44aecf
facahto <fcahyohartoto@gmail.com>
2023-12-27 16:14:55 +0700

    Day06

---
+++ Day006/OperatorOverload2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+class Program
+{
+	static void Main()
+	{
+		Car cara = new Car(5000,10,"a");
+		Car carb = new Car(3000,11,"b");
+		Car carc = new Car(2000,12,"c");
+		Car card = cara+carb+carc;
+		card.price.Dump();
+	}
+}
+// 
+public class Car : IAdditionOperators<Car,Car,Car>
+{
+	public int year;
+	public int price;
+	public string name;
+	public Car(int price, int year, string name)
+	{
+		this.price = price;
+		this.year = year;
+		this.name = name;
+	}
+	public static Car operator +(Car left, Car right)
+	{
+		Car resultCar = new Car(left.price+right.price,0,"");
+		return resultCar;
+	}
+	
+}
+public static class StringExtention
+{
+	public static void Dump(this object x)
+	{
+		Console.WriteLine(x);
+	}
+}
--- Day006/OperatorOverloading/Program.cs
+++ Day006/OperatorOverloading/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+
+// operator hanya bisa digunakan untuk tipe data
+// bisa untuk class tapi dengan adanya kondisi
+class Calculator<T> where T:IAdditionOperators<T,T,T>
+{
+	public T Addition(T a, T b)
+	{
+		return a+b;
+	}
+}
+class Program
+{
+	static void Main()
+	{
+		Calculator<int> calculator = new();
+		int result = calculator.Addition(3,4);
+		result.GetType().Dump();
+		result.Dump();
+}
+}
+public static class StringExtention
+{
+	public static void Dump(this object x)
+	{
+		Console.WriteLine(x);
+	}
+}
