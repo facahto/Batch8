1549dc0cb18618b35e81e6c93f776b9d0814294b
facahto <fcahyohartoto@gmail.com>
2023-12-27 17:05:44 +0700

    Day06

---
+++ Day006/Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+	static void Main()
+	{
+		//buat instance dari publisher sama subcriber
+		GameController game = new GameController();
+		SMS sms = new SMS();
+		
+		game.mySubscriber = sms.SendSMS;
+		game.UpdateGameStatus();
+	}
+}
+//DELEGATE
+public delegate void MyDelegate(string message);
+
+//PUBLISHER
+class GameController
+{
+	public MyDelegate mySubscriber;
+	public GameController(){}
+	public void UpdateGameStatus()
+	{
+		string status = "game Running";
+		mySubscriber.Invoke(status);
+	}
+}
+
+//SUBSCRIBER
+class SMS
+{
+	public void SendSMS(string message)
+	{
+		Console.WriteLine("SMS " + message);
+	}
+}
---
+++ Day006/Delegate2/Program.cs
@@ -0,0 +1,62 @@
+﻿//SUBSCRIBER
+class SMS
+{
+	public void SendSMS(string message)
+	{
+		Console.WriteLine("SMS " + message);
+	}
+}
+class Email
+{
+	public void SendEmail(string message)
+	{
+		Console.WriteLine("email " + message);
+	}
+}
+class DataBase
+{
+	public void SendDatabase(string message)
+	{
+		Console.WriteLine("database " + message);
+	}
+}
+class API
+{
+	public void SendAPI(string message)
+	{
+		Console.WriteLine("API " + message);
+	}
+}
+//PUBLISHER
+class GameController
+{
+	public MyDelegate mySubscriber;
+	public GameController()
+	{
+		
+	}
+	public void UpdateGameStatus()
+	{
+		string status = "game Running";
+		mySubscriber.Invoke(status);
+	}
+}
+public delegate void MyDelegate(string message);
+class Program
+{
+	static void Main()
+	{
+		//instance untuk publisher sama subscriber
+		GameController game = new GameController();
+		SMS sms = new SMS();
+		Email email = new Email();
+		DataBase database = new DataBase();
+		API api = new API();
+		game.mySubscriber = sms.SendSMS;
+		game.mySubscriber += email.SendEmail;
+		game.mySubscriber += database.SendDatabase;
+		game.mySubscriber += api.SendAPI;
+		//Invoke
+		game.UpdateGameStatus();
+	}
+}
