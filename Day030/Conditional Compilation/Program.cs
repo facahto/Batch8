9674d8b65218e2c7fbe98634687b26a3254e5157
facahto <fcahyohartoto@gmail.com>
2024-01-17 14:26:04 +0700

    Day030

---
+++ Day030/Conditional Compilation/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+	static void Main()
+	{
+		#if BAMBANG
+		#region Bambang
+		{
+			Console.WriteLine("BAMBANG");
+		}
+		#endregion
+		#elif YANTO
+		#region Yanto
+		{
+			Console.WriteLine("YANTO");
+		}
+		#endregion
+		#else
+		#region Selamet
+		{
+			Console.WriteLine("SELAMET");
+		}
+		#endregion
+		#endif
+	}
+}
