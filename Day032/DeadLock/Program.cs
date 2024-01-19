f0e7e043b324adf2af8ee379d50d546b3e2977c1
facahto <fcahyohartoto@gmail.com>
2024-01-19 14:59:15 +0700

    Day032

---
+++ Day032/DeadLock/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+	static async void Main()
+	{
+		Console.WriteLine("Main Method Started");
+		Task task1 = null;
+		Task task2 = null;
+		task1 = Task.Run(async() => {
+			Console.WriteLine("Task 1 started");
+			await Task.Delay(10);
+			
+			Console.WriteLine("Task 1 started");
+			await task2;
+			
+			Console.WriteLine("Task 1 completed");
+		});
+		task2 = Task.Run(async() => {
+			Console.WriteLine("Task 1 started");
+			await Task.Delay(10);
+			
+			Console.WriteLine("Task 1 started");
+			await task1;
+			
+			Console.WriteLine("Task 1 completed");
+		});
+		await Task.WhenAll(task1,task2);
+		
+		Console.WriteLine("Main Method completed");
+		Console.ReadKey();
+	}
+}
---
+++ Day032/DeadLock2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+static class Program
+{
+	static async Task Main(string[] args)
+	{
+		Console.WriteLine("Main Method Started");
+
+		var task1 = Task.Run(async() => 
+		{
+			Console.WriteLine("Task1 started");
+			await Task.Delay(1000);
+			Console.WriteLine("Task1 waiting Task2");
+		});
+		
+		var task2 = Task.Run(async() => 
+		{
+			Console.WriteLine("Task2 started");
+			await Task.Delay(1000);
+			Console.WriteLine("Task2 waiting Task1");
+		});
+		
+		await Task.WhenAll(task1,task2);
+
+		Console.WriteLine("All tasks completed");
+		Console.ReadKey();
+	}
+}
